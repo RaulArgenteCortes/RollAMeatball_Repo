be4840cb73b908248323e76ad1cdbf4f93780b8d
uluna99 <uluna099@gmail.com>
2024-11-27 22:18:06 +0100

    winpanel

---
+++ RollAMeatball/Assets/Scripts/WinPanel.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class PanelVictoria : MonoBehaviour
+{
+    public GameObject panelVictoria;  
+    private string Level3;    
+
+    void Start()
+    {
+        panelVictoria.SetActive(false); 
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))  
+        {
+            MostrarPantallaVictoria();
+        }
+    }
+
+    // Función que muestra la pantalla de victoria
+    public void MostrarPantallaVictoria()
+    {
+        panelVictoria.SetActive(true);   // Activa el panel de victoria
+        Time.timeScale = 0f;            
+    }
+
+    // Función para volver al menú principal
+    public void VolverAlMenu()
+    {
+        Time.timeScale = 1f;  
+        SceneManager.LoadScene("MainMenu");  
+    }
+
+    public void SiguienteNivel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level3");
+    }
+
+
+}
