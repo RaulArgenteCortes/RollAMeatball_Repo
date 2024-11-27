d9c81a4cde919a981ee92d34b88d9ffab99e7c91
Capuchinochino <deltoronando@gmail.com>
2024-11-27 23:27:22 +0100

    Level 2 Completado

--- RollAMeatball/Assets/Scripts/CollisionDeath.cs
+++ RollAMeatball/Assets/Scripts/CollisionDeath.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class CollisionDeath : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject finishCanvas;
     void Start()
     {
 
@@ -17,5 +18,18 @@ public class CollisionDeath : MonoBehaviour
             //Debug.Log("Collision with an Obstacle");
             SceneManager.LoadScene(2);
         }
-    } 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object collided with has the "Finish" tag
+        if (other.CompareTag("Finish"))
+        {
+            // Show the Canvas
+            finishCanvas.SetActive(true);
+
+            // Optionally, disable the player or pause the game
+            // Time.timeScale = 0; // Uncomment to pause the game
+        }
+    }
+
 }
